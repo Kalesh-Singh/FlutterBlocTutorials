248bc59d8436385a863817599469056951253163
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-02-25 10:33:39 -0800

    Counter Page

---
+++ 01-CounterApp/counter_app/lib/pages/counter/counter_page.dart
@@ -0,0 +1,45 @@
+import 'package:counter_app/blocs/counter/counter_bloc.dart';
+import 'package:counter_app/blocs/counter/counter_event.dart';
+import 'package:counter_app/pages/counter/widgets/counter_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count) {
+          return Center(
+            child: CounterText(),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                counterBloc.add(CounterEvent.increment);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () {
+                counterBloc.add(CounterEvent.decrement);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
