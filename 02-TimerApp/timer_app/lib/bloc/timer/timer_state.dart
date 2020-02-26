876a1aefbcd8b9681e6f5203a9bde70dc579a447
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-02-25 18:45:08 -0800

    Timer States Documentation

--- 02-TimerApp/timer_app/lib/bloc/timer/timer_state.dart
+++ 02-TimerApp/timer_app/lib/bloc/timer/timer_state.dart
@@ -1,5 +1,21 @@
 import 'package:equatable/equatable.dart';
 
+/*
+Ready — ready to start counting down from the specified duration.
+Running — actively counting down from the specified duration.
+Paused — paused at some remaining duration.
+Finished — completed with a remaining duration of 0.
+
+Each of these states will have an implication on what the user sees.
+For example:
+  1. If the state is “ready,” the user will be able to start the timer.
+  2. If the state is “running,” the user will be able to pause and reset
+     the timer as well as see the remaining duration.
+  3. If the state is “paused,” the user will be able to resume the timer
+     and reset the timer.
+  4. If the state is “finished,” the user will be able to reset the timer.
+ */
+
 abstract class TimerState extends Equatable {
   final int duration;
 
