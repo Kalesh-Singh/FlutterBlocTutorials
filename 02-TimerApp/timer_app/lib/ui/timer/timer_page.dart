f55ccc13881cb451ec7547de220484e899f43bea
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-02-29 10:10:49 -0800

    Added waves background to timer page

--- 02-TimerApp/timer_app/lib/ui/timer/timer_page.dart
+++ 02-TimerApp/timer_app/lib/ui/timer/timer_page.dart
@@ -4,6 +4,8 @@ import 'package:timer_app/bloc/timer/timer_bloc.dart';
 import 'package:timer_app/bloc/timer/timer_state.dart';
 import 'package:timer_app/ui/timer/widgets/actions.dart';
 
+import 'widgets/waves_background.dart';
+
 class TimerPage extends StatelessWidget {
   static const TextStyle timerTextStyle = TextStyle(
     fontSize: 60,
@@ -14,31 +16,34 @@ class TimerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Flutter Timer')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 100.0),
-              child: Center(child:
-                  BlocBuilder<TimerBloc, TimerState>(builder: (context, state) {
-                final String minutesStr =
-                    (state.duration ~/ 60).toString().padLeft(2, '0');
-                final String secondsStr =
-                    (state.duration % 60).toString().padLeft(2, '0');
-                return Text(
-                  '$minutesStr:$secondsStr',
-                  style: TimerPage.timerTextStyle,
-                );
-              })),
-            ),
-            BlocBuilder<TimerBloc, TimerState>(
-              // We don't want the buttons to be rebuilt on every Tick
-              condition: (previousState, state) =>
-                  state.runtimeType != previousState.runtimeType,
-              builder: (context, state) => TimerActions(),
-            ),
-          ],
-        ));
+        body: Stack(children: [
+          Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: BlocBuilder<TimerBloc, TimerState>(
+                    builder: (context, state) {
+                  final String minutesStr =
+                      (state.duration ~/ 60).toString().padLeft(2, '0');
+                  final String secondsStr =
+                      (state.duration % 60).toString().padLeft(2, '0');
+                  return Text(
+                    '$minutesStr:$secondsStr',
+                    style: TimerPage.timerTextStyle,
+                  );
+                })),
+              ),
+              BlocBuilder<TimerBloc, TimerState>(
+                // We don't want the buttons to be rebuilt on every Tick
+                condition: (previousState, state) =>
+                    state.runtimeType != previousState.runtimeType,
+                builder: (context, state) => TimerActions(),
+              ),
+            ],
+          ),
+        ]));
   }
 }
