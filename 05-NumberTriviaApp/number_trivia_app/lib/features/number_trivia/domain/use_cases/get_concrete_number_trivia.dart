4ce8da1bce20e08e6063603520ea26b7ce448c82
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-03-01 11:49:35 -0800

    Get Concrete Number Usecase and Test

---
+++ 05-NumberTriviaApp/number_trivia_app/lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+import 'package:numbertriviaapp/core/error/failure.dart';
+import 'package:numbertriviaapp/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:numbertriviaapp/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  const GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    @required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia((number));
+  }
+}
