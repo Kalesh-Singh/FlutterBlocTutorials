eb4560818d8d612449e720618c0720b32e8bed70
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-02-29 22:37:37 -0800

    List App

--- 03-InfiniteListApp/infinite_list_app/lib/main.dart
+++ 03-InfiniteListApp/infinite_list_app/lib/main.dart
@@ -1,61 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:infinitelistapp/ui/inifinite_list.dart';
 
-void main() => runApp(MyApp());
+import 'blocs/posts/post_bloc.dart';
+import 'blocs/posts/post_events.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(ListApp());
+
+class ListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      title: 'Flutter Infinite Scroll',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Posts'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: BlocProvider(
+          create: (context) =>
+              PostBloc(httpClient: http.Client())..add(Fetch()),
+          child: InfiniteList(),
+        ),
       ),
     );
   }
