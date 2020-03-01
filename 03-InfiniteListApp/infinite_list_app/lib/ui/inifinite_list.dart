a140be68cd50aa4dadfe18dfed7d82f8e395013c
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-02-29 22:29:15 -0800

    Infinite List

--- 03-InfiniteListApp/infinite_list_app/lib/ui/inifinite_list.dart
+++ 03-InfiniteListApp/infinite_list_app/lib/ui/inifinite_list.dart
@@ -1,9 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinitelistapp/blocs/posts/post_bloc.dart';
+import 'package:infinitelistapp/blocs/posts/post_events.dart';
+import 'package:infinitelistapp/blocs/posts/post_state.dart';
+import 'package:infinitelistapp/ui/bottom_loader.dart';
+import 'package:infinitelistapp/ui/post_tile.dart';
 
-class HomePage extends StatelessWidget {
+class InfiniteList extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    
+  _InfiniteListState createState() => _InfiniteListState();
+}
+
+class _InfiniteListState extends State<InfiniteList> {
+  final _scrollController = ScrollController();
+  final _scrollThreshold = 200.0;
+  PostBloc _postBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _postBloc = BlocProvider.of<PostBloc>(context);
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PostBloc, PostState>(
+      builder: (BuildContext context, PostState state) {
+        if (state is PostUninitialized) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is PostError) {
+          return Center(
+            child: Text('Failed to fetch posts'),
+          );
+        }
+        if (state is PostLoaded) {
+          if (state.posts.isEmpty) {
+            return Center(
+              child: Text('No posts'),
+            );
+          }
+
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return index >= state.posts.length
+                  ? BottomLoader()
+                  : PostTile(post: state.posts[index]);
+            },
+            itemCount: state.hasReachedMax
+                ? state.posts.length
+                : state.posts.length + 1,
+            controller: _scrollController,
+          );
+        }
+        return Center();
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      _postBloc.add(Fetch());
+    }
+  }
 }
