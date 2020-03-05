965780fcede0bfaa38ef551b5c7f053a8ef0a2df
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-03-04 18:20:59 -0800

    Top Half of UI and Input Field

--- 05-NumberTriviaApp/number_trivia_app/lib/features/number_trivia/presentaion/pages/number_trivia_page.dart
+++ 05-NumberTriviaApp/number_trivia_app/lib/features/number_trivia/presentaion/pages/number_trivia_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:numbertriviaapp/features/number_trivia/presentaion/bloc/number_trivia_bloc.dart';
+import 'package:numbertriviaapp/features/number_trivia/presentaion/bloc/number_trivia_state.dart';
+import 'package:numbertriviaapp/features/number_trivia/presentaion/widgets/loading_widget.dart';
+import 'package:numbertriviaapp/features/number_trivia/presentaion/widgets/message_display.dart';
+import 'package:numbertriviaapp/features/number_trivia/presentaion/widgets/trivia_display.dart';
 
 import '../../../../injection_container.dart';
 
@@ -21,36 +25,70 @@ class NumberTriviaPage extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: <Widget>[
-                // Top Half
-                SizedBox(height: 10),
-                Container(
-                  height: MediaQuery.of(context).size.height / 3,
-                  child: Placeholder(),
-                ),
-                SizedBox(height: 20),
-                // Bottom Half
-                Column(
-                  children: <Widget>[
-                    // TextField
-                    Placeholder(fallbackHeight: 40),
-                    SizedBox(height: 10),
-                    // Buttons
-                    Row(
-                      children: <Widget>[
-                        Expanded(child: Placeholder(fallbackHeight: 30)),
-                        SizedBox(width: 10),
-                        Expanded(child: Placeholder(fallbackHeight: 30)),
-                      ],
-                    ),
-                  ],
-                ),
-                // Bottom Half
-              ],
-            ),
+          child: Column(
+            children: <Widget>[
+              // Top Half
+              SizedBox(height: 10),
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (BuildContext context, state) {
+                  if (state is TriviaEmpty) {
+                    return MessageDisplay(message: 'Start searching!');
+                  } else if (state is TriviaLoadInProgress) {
+                    return LoadingWidget();
+                  } else if (state is TriviaLoadSuccess) {
+                    return TriviaDisplay(numberTrivia: state.trivia);
+                  } else if (state is TriviaLoadFailure) {
+                    return MessageDisplay(message: state.message);
+                  }
+                },
+              ),
+              SizedBox(height: 20),
+              // Bottom Half
+              TriviaControls(),
+              // Bottom Half
+            ],
           ),
+        ),
       ),
+    );
+  }
+}
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _TriviaControlsState createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  String inputStr;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a number',
+          ),
+          onChanged: (value) {
+            inputStr = value;
+          },
+        ),
+        SizedBox(height: 10),
+        // Buttons
+        Row(
+          children: <Widget>[
+            Expanded(child: Placeholder(fallbackHeight: 30)),
+            SizedBox(width: 10),
+            Expanded(child: Placeholder(fallbackHeight: 30)),
+          ],
+        ),
+      ],
     );
   }
 }
